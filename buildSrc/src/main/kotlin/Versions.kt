c40ffb4917133ef53d4aa4528e9b8864f0cd1d2a
lt <lt.dygzs@qq.com>
2022-11-29 23:20:37 +0800

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -18,4 +18,4 @@ const val kotlinVersion = "1.7.20"//kotlin版本
 const val composeVersion = "1.2.1"//compose版本
 const val composeCompilerVersion = "1.3.2"//compose编译版本
 const val coroutinesVersion = "1.6.4"//协程版本
-const val githubVersion = "1.3.3.12"//github发布版本
+const val githubVersion = composeVersion//github发布版本
