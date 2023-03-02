94f513edfe7b82abc17c010e926ea36021aca062
lt_taozi <1764780379@qq.com>
2023-03-02 17:52:59 +0800

    完善js的jump(还有问题)

--- common_app/src/jsMain/kotlin/com/lt/common_app/base/BaseComposeActivity.kt
+++ common_app/src/jsMain/kotlin/com/lt/common_app/base/BaseComposeActivity.kt
@@ -1,13 +1,20 @@
 package com.lt.common_app.base
 
 import androidx.compose.runtime.*
+import com.lt.common_app.ComposePagerActivity
+import com.lt.common_app.DateSelectorA
 import com.lt.common_app.MainActivity
-import kotlinx.coroutines.*
+import com.lt.common_app.MainListActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 import kotlin.reflect.KClass
 
 /**
@@ -43,7 +50,12 @@ actual abstract class BaseComposeActivity {
     }
 
     actual fun jump(clazz: KClass<out BaseComposeActivity>) {
-//        _activityStack.add(clazz.)
+        val constructor = this::class.js.asDynamic().prototype.constructor
+        val a = constructor.call() as BaseComposeActivity
+//        val a= DateSelectorA()
+//        val a = js("new ${clazz.simpleName!!}()") as BaseComposeActivity
+//        val a=   kotlinx.browser.window[clazz.simpleName!!].invoke() as BaseComposeActivity
+        _activityStack.add(a)
     }
 
     companion object {
