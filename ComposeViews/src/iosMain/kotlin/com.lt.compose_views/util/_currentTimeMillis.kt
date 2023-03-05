081f6e1b7634bee6714576e9c19d701ec5b50e56
lt <lt.dygzs@qq.com>
2023-03-05 13:29:33 +0800

    Update _currentTimeMillis.kt

--- ComposeViews/src/iosMain/kotlin/com.lt.compose_views/util/_currentTimeMillis.kt
+++ ComposeViews/src/iosMain/kotlin/com.lt.compose_views/util/_currentTimeMillis.kt
@@ -4,4 +4,4 @@ package com.lt.compose_views.util
 /**
  * 获取当前时间戳(ms)
  */
-actual fun _currentTimeMillis(): Long = TODO()
+actual fun _currentTimeMillis(): Long = (Date().timeIntervalSince1970 * 1000).toLong
