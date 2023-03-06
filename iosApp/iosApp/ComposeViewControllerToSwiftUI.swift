933ad876c3443bd749ff9933e9214ab6d6043b07
lt_taozi <1764780379@qq.com>
2023-03-06 22:37:22 +0800

    ios可用

--- iosApp/iosApp/ComposeViewControllerToSwiftUI.swift
+++ iosApp/iosApp/ComposeViewControllerToSwiftUI.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ios_shared
+import ComposeViews
 
 struct ComposeViewControllerToSwiftUI: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
