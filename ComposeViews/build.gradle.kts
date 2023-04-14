712487ef55a25a2a9c6bd219abb78a43c35db0d8
lt_taozi <1764780379@qq.com>
2023-04-14 23:34:44 +0800

    Update build.gradle.kts

--- ComposeViews/build.gradle.kts
+++ ComposeViews/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 group = "io.github.ltttttttttttt"
 //上传到mavenCentral命令: ./gradlew publishAllPublicationsToSonatypeRepository
 //mavenCentral后台: https://s01.oss.sonatype.org/#stagingRepositories
-version = "1.3.11"
+version = "1.4.0"
 
 kotlin {
     android {
