018f25082dc99401aa172d4adbf3056c8d987b3d
lt <lt.dygzs@qq.com>
2023-05-24 22:02:32 +0800

    Update build.gradle.kts

--- ComposeViews/build.gradle.kts
+++ ComposeViews/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 group = "io.github.ltttttttttttt"
 //上传到mavenCentral命令: ./gradlew publishAllPublicationsToSonatypeRepository
 //mavenCentral后台: https://s01.oss.sonatype.org/#stagingRepositories
-version = "1.4.0.2"
+version = "1.4.0.3"
 
 kotlin {
     android {
