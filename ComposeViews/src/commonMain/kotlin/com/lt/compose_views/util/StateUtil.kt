577d4f930988d742ababd07e05dab15c89d04562
lt <lt.dygzs@qq.com>
2023-05-30 17:41:32 +0800

    Update StateUtil.kt

--- ComposeViews/src/commonMain/kotlin/com/lt/compose_views/util/StateUtil.kt
+++ ComposeViews/src/commonMain/kotlin/com/lt/compose_views/util/StateUtil.kt
@@ -33,3 +33,15 @@ import androidx.compose.runtime.remember
  */
 @Composable
 fun <T> rememberMutableStateOf(value: T): MutableState<T> = remember { mutableStateOf(value) }
+
+@Composable
+fun <T> rememberMutableStateOf(key1: Any?, value: T): MutableState<T> =
+    remember(key1 = key1) { mutableStateOf(value) }
+
+@Composable
+fun <T> rememberMutableStateOf(key1: Any?, key2: Any?, value: T): MutableState<T> =
+    remember(key1 = key1, key2 = key2) { mutableStateOf(value) }
+
+@Composable
+fun <T> rememberMutableStateOf(key1: Any?, key2: Any?, key3: Any?, value: T): MutableState<T> =
+    remember(key1 = key1, key2 = key2, key3 = key3) { mutableStateOf(value) }
