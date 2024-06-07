8c0dabd2f7ed840a3bd2a511c3a75005692c4327
lt <lt.dygzs@qq.com>
2024-06-07 19:11:02 +0800

    优化rememberMutableStateOf,提升性能

--- ComposeViews/src/commonMain/kotlin/com/lt/compose_views/util/StateUtil.kt
+++ ComposeViews/src/commonMain/kotlin/com/lt/compose_views/util/StateUtil.kt
@@ -16,10 +16,7 @@
 
 package com.lt.compose_views.util
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 
 /**
@@ -33,19 +30,78 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
  * Quick use remember { mutableStateOf(T) }
  */
 @Composable
+inline fun <T> rememberMutableStateOf(
+    crossinline initValue: @DisallowComposableCalls () -> T
+) = remember { mutableStateOf(initValue()) }
+
+@Composable
+inline fun <T> rememberMutableStateOf(
+    key1: Any?,
+    crossinline initValue: @DisallowComposableCalls () -> T
+): MutableState<T> = remember(key1 = key1) { mutableStateOf(initValue()) }
+
+@Composable
+inline fun <T> rememberMutableStateOf(
+    key1: Any?,
+    key2: Any?,
+    crossinline initValue: @DisallowComposableCalls () -> T
+): MutableState<T> = remember(key1 = key1, key2 = key2) { mutableStateOf(initValue()) }
+
+@Composable
+inline fun <T> rememberMutableStateOf(
+    key1: Any?,
+    key2: Any?,
+    key3: Any?,
+    crossinline initValue: @DisallowComposableCalls () -> T
+): MutableState<T> = remember(key1 = key1, key2 = key2, key3 = key3) { mutableStateOf(initValue()) }
+
+@Composable
+fun <T> rememberMutableStateListOf(): SnapshotStateList<T> = remember { SnapshotStateList() }
+
+@Composable
+inline fun <T> rememberMutableStateListOf(
+    crossinline initValue: @DisallowComposableCalls SnapshotStateList<T>.() -> Unit
+): SnapshotStateList<T> = remember { SnapshotStateList<T>().apply(initValue) }
+
+@Deprecated(
+    "Need to use a function with the same name as lambda for higher performance", ReplaceWith(
+        "rememberMutableStateOf { value }",
+        "androidx.compose.runtime.remember",
+        "androidx.compose.runtime.mutableStateOf"
+    )
+)
+@Composable
 fun <T> rememberMutableStateOf(value: T): MutableState<T> = remember { mutableStateOf(value) }
 
+@Deprecated(
+    "Need to use a function with the same name as lambda for higher performance", ReplaceWith(
+        "rememberMutableStateOf(key1) { value }",
+        "androidx.compose.runtime.remember",
+        "androidx.compose.runtime.mutableStateOf"
+    )
+)
 @Composable
 fun <T> rememberMutableStateOf(key1: Any?, value: T): MutableState<T> =
     remember(key1 = key1) { mutableStateOf(value) }
 
+@Deprecated(
+    "Need to use a function with the same name as lambda for higher performance", ReplaceWith(
+        "rememberMutableStateOf(key1, key2) { value }",
+        "androidx.compose.runtime.remember",
+        "androidx.compose.runtime.mutableStateOf"
+    )
+)
 @Composable
 fun <T> rememberMutableStateOf(key1: Any?, key2: Any?, value: T): MutableState<T> =
     remember(key1 = key1, key2 = key2) { mutableStateOf(value) }
 
+@Deprecated(
+    "Need to use a function with the same name as lambda for higher performance", ReplaceWith(
+        "rememberMutableStateOf(key1, key2, key3) { value }",
+        "androidx.compose.runtime.remember",
+        "androidx.compose.runtime.mutableStateOf"
+    )
+)
 @Composable
 fun <T> rememberMutableStateOf(key1: Any?, key2: Any?, key3: Any?, value: T): MutableState<T> =
     remember(key1 = key1, key2 = key2, key3 = key3) { mutableStateOf(value) }
-
-@Composable
-fun <T> rememberMutableStateListOf(): SnapshotStateList<T> = remember { SnapshotStateList() }
